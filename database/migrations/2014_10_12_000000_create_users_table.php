469eca9a9e4226025fcd810a3ebb98959943de5e
haq47 <freelanc47@gmail.com>
2021-03-10 22:10:12 +0600

    add user and role migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedInteger('role_id') -> default(2);
             $table->string('email')->unique();
+            $table->string('phone_number')->unique() -> nullable();
+            $table->string('username')->unique() -> nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo') -> default('avatar.jpg');
+            $table->boolean('status') -> default(true);
             $table->rememberToken();
             $table->timestamps();
         });
